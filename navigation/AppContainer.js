6d1f4446ca4b14afd7f5862c8575c7fd489c97c1
Lucien <lygm1062@gmail.com>
2018-12-09 21:48:52 +0800

    使用自定义组件替换标题

--- navigation/AppContainer.js
+++ navigation/AppContainer.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, Image, View, Text } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
+
+class LogoTitle extends React.Component {
+    render() {
+        return (
+            <Image
+                source={require('./spiro.png')}
+                style={{ width: 30, height: 30 }}
+            />
+        )
+    }
+}
 
 class HomeScreen extends React.Component {
     static navigationOptions = {
-        title: 'Home',
+        headerTitle: <LogoTitle />
     }
 
     render() {

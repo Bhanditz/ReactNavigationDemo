16424f76b70070c503a3a3ffcb1b2e476039878f
Lucien <lygm1062@gmail.com>
2018-12-23 11:39:43 +0800

    自定义TabNavigator外观

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 class HomeScreen extends React.Component {
@@ -22,9 +23,30 @@ class SettingsScreen extends React.Component {
     }
 }
 
-const TabNavigator = createBottomTabNavigator({
-    Home: HomeScreen,
-    Settings: SettingsScreen,
-});
+const TabNavigator = createBottomTabNavigator(
+    {
+        Home: HomeScreen,
+        Settings: SettingsScreen,
+    },
+    {
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+                const {routeName}  = navigation.state;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = `information-circle${focused ? '' : '-outline'}`;
+                } else if (routeName === 'Settings') {
+                    iconName = `options${focused ? '' : '-outline'}`;
+                }
+
+                return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+    }
+);
 
 export default createAppContainer(TabNavigator);

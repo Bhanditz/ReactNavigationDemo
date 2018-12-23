ad83311dd7b6dd64c9b7ffd39173295c5163fbc5
Lucien <lygm1062@gmail.com>
2018-12-23 11:53:38 +0800

    每个 Tab 的 stack navigator

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import {
+    createBottomTabNavigator,
+    createAppContainer,
+    createStackNavigator,
+} from 'react-navigation';
 
+class DetailsScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>Details!</Text>
+            </View>
+        );
+    }
+}
 class HomeScreen extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>Home!</Text>
                 <Button
-                    title='Go to Settings'
-                    onPress={() => this.props.navigation.navigate('Settings')}
+                    title='Go to Details'
+                    onPress={() => this.props.navigation.navigate('Details')}
                 />
             </View>
         )
@@ -23,18 +36,28 @@ class SettingsScreen extends React.Component {
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Text>Settings!</Text>
                 <Button
-                    title='Go to Home'
-                    onPress={() => this.props.navigation.navigate('Home')}
+                    title='Go to Details'
+                    onPress={() => this.props.navigation.navigate('Details')}
                 />
             </View>
         )
     }
 }
 
+const HomeStack = createStackNavigator({
+    Home: HomeScreen,
+    Details: DetailsScreen,
+});
+  
+const SettingsStack = createStackNavigator({
+    Settings: SettingsScreen,
+    Details: DetailsScreen,
+});
+
 const TabNavigator = createBottomTabNavigator(
     {
-        Home: HomeScreen,
-        Settings: SettingsScreen,
+        Home: HomeStack,
+        Settings: SettingsStack,
     },
     {
         defaultNavigationOptions: ({ navigation }) => ({
